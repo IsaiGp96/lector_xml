cd74970a3a2c21a3d402ae1a8589e07f1f213cf2
IsaiGp96 <abraham_gp96@outlook.com>
2024-11-11 17:30:10 -0600

    Actualizacion para pruebas del script de analisis

--- calc/system.py
+++ calc/system.py
@@ -1,14 +1,18 @@
 import os
 import xml.etree.ElementTree as ET
-ARVCHIVE_FOLDER = "temp/files_xml"
+
+ARCHIVE_FOLDER = r"temp\files_xml"
 
 class Archivo:
-    def analzar():
+    def analizar(self):
         pass
 
 class Carpeta:
-    def carpeta_temp():
-        if os.listdir(ARVCHIVE_FOLDER):
-            for file in ARVCHIVE_FOLDER:
-                return file
-    
+    def carpeta_temp(self):
+        if os.path.exists(ARCHIVE_FOLDER) and os.listdir(ARCHIVE_FOLDER):
+            for file in os.listdir(ARCHIVE_FOLDER):
+                print(file)
+
+## Pruebas pruebas
+carpeta = Carpeta()
+carpeta.carpeta_temp()
