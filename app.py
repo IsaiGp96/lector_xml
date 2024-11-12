9239166ccbd6c4703c0584c36783a04835bddab0
IsaiGp96 <abraham_gp96@outlook.com>
2024-11-12 13:26:14 -0600

    Se agrega funcionalidad de toastr

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+from flask_toastr import Toastr
 
 app = Flask(__name__, static_url_path='/static')
+toastr = Toastr(app)
 UPLOAD_FOLDER = 'temp/files_xml'
 ALLOWED_EXTENSIONS = {'xml', 'XML'}
 
@@ -26,9 +28,12 @@ def upload_file():
             return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
+
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
+            flash({'title': "ERROR", 
+                   'message': "Sin archivo seleccionado",
+                   'TOASTR_POSITION_CLASS': "toast-top-center"}, 'error',)
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -36,4 +41,5 @@ def upload_file():
             return redirect(url_for('upload_file', name=filename))
 
 if __name__ == '__main__':
+    app.secret_key = "SECRET KEY"
     app.run(host="127.0.0.1", port="5000",debug=True)
